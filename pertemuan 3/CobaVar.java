08b6ffac0b06358864408e2cdf67e50a42705659
fauzie543 <fauziekurniawan0@gmail.com>
2021-09-14 13:39:07 +0700

    Pertemuan 3

---
+++ pertemuan 3/CobaVar.java
@@ -0,0 +1,12 @@
+public class CobaVar {
+    public static void main(String[] args) {
+        String namaDepan = "Sri Fauzie Kurniawan";
+        int umur = 18;
+        double tinggiBadan = 165;
+        int beratBadan = 57;
+        System.out.println("Nama Saya " + namaDepan);
+        System.out.println("Umur Saya " + umur);
+        System.out.println("Tinggi Saya " + tinggiBadan);
+        System.out.println("Berat Badan Saya " + beratBadan);
+    }  
+}
