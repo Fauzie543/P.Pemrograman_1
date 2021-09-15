6549dcbae34793c63b5ae49bc3cdbfaac34b79f9
fauzie543 <fauziekurniawan0@gmail.com>
2021-09-15 10:28:52 +0700

    Tugas1

---
+++ pertemuan 3/Tugas1.java
@@ -0,0 +1,20 @@
+public class Tugas1 {
+    public static void main(String[] args) {
+        System.out.println("Identias Mahasiswa UNY");
+        System.out.println(" ");
+        String namaLengkap = "Sri Fauzie Kurniawan";
+        long NIM = 21537144030L;
+        int Angkatan = 2021;
+        String ProgramStudi = "Teknologi Informasi";
+        String Jurusan = "JPTEI";
+        String Fakutas = "Teknik";
+        double IPKharapan = 3.65;
+        System.out.println("Nama Lengkap = " + namaLengkap);
+        System.out.println("NIM = " + NIM);
+        System.out.println("Angkatan = " + Angkatan);
+        System.out.println("Program Studi = " + ProgramStudi);
+        System.out.println("Jurusan = " + Jurusan);
+        System.out.println("Fakultas = " + Fakutas);
+        System.out.println("IPK yang di harapkan = " + IPKharapan);
+    }  
+}
---
+++ pertemuan 3/Tugas1.py
@@ -0,0 +1,17 @@
+namaLengkap = "Sri Fauzie Kurniawan"
+nim = 21537144030
+angkatan = 2021
+prodi = "Teknologi Informasi"
+jurusan = "JPTEI"
+fakultas = "Teknik"
+IPKharapan = 3.65
+print("Identias Mahasiswa UNY")
+print(" ")
+print("Nama Lengkap = " + namaLengkap)
+print("NIM = " + str(nim))
+print("Angkatan = " + str(angkatan))
+print("Program Studi = " + str(prodi))
+print("Jurusan = " + jurusan)
+print("Fakutas = " + fakultas)
+print("IPK Harapan = " + str(IPKharapan))
+
